5f0686f5e3c4c36d05ad04ea73c4ee6e19490ea8
Alex Nesvit <a.nesvit@dataduck.com>
2022-02-09 10:17:45 +0300

    added opacy

---
+++ index.js
@@ -0,0 +1,50 @@
+class Opacy {
+    constructor(selector) {
+        this.target = document.querySelectorAll(selector)
+
+        let options = { threshold: [0.5] }
+        let observer = new IntersectionObserver(this.onEntry.bind(this), options)
+
+
+        for (const elem of this.target) {
+            observer.observe(elem)
+        }
+    }
+
+    onEntry(entry) {
+        entry.forEach(change => {
+            console.log(this)
+            const scrollHandle = this.handler(change.target)
+    
+            if (change.isIntersecting) {
+                document.addEventListener('scroll', scrollHandle)
+            } else {
+                document.removeEventListener('scroll', scrollHandle)
+            }
+        })
+    }
+
+    handler(target) {
+        return () => {
+            const rect = target.getBoundingClientRect()
+        
+            const percent = this.getPercent(rect.y + rect.height / 2, innerHeight / 2)
+            
+            target.style.opacity = Math.abs((percent - 100) / 100)
+        }
+    }
+
+    getPercent(current, total) {
+        let pos = 0
+    
+        if (current < total) {
+            pos = total - current
+        } else {
+            pos = Math.abs(innerHeight - current - total)
+        }
+    
+        return (pos / total) * 100
+    }
+}
+
+new Opacy('.box')
